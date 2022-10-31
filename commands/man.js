ca8c0f4a74fa238bb44ea5a26b5253b5ab5f86b4
MatterMr <48224224+MatterMr@users.noreply.github.com>
2022-10-31 17:31:14 -0600

    Updated man cmd to allow options and depth.

--- commands/man.js
+++ commands/man.js
@@ -1,24 +1,37 @@
 module.exports = {
 	'name': 'man',
-	'desc': 'returns a manual',
-	'params': '(commandname, -d?, -p?)',
+	'synopsis':'man [OPTION] [COMMAND_NAME]',
+	'description': `
+        Print manual for specifyed COMMAND or execute acording to OPTIONS.
+        -l : List all available commands
+        -s : Display only the synopsis of the command
+        -d : Dsiplay only the description of the command`,
 	async execute(args, handler) {
+		const shift = args[1] == undefined ? 0 : 1;
 
-		if (!handler.commands.has(args[0])) return;
-
-		const description = handler.commands.get(args[0]).desc;
-		const parameters = handler.commands.get(args[0]).params;
-
-		if (args.includes('-d')) {
-			console.log(`desc: ${description}`);
-			return;
+		if (args[0] == '-l') {
+			let formatted_string = 'Commands:\n';
+			for (const [key] of handler.commands) {
+				formatted_string += (`    ${key}\n`);
+			}
+			console.log(formatted_string);
 		}
-		if (args.includes('-p')) {
-			console.log(`params: ${parameters}`);
-			return;
+		if (handler.commands.has(args[shift])) {
+			const name = handler.commands.get(args[shift]).name;
+			const synopsis = handler.commands.get(args[shift]).synopsis;
+			const description = handler.commands.get(args[shift]).description;
+
+			switch (args[0]) {
+			case '-s':
+				console.log(`SYNOPSIS\n\t${synopsis}`);
+				break;
+			case '-d':
+				console.log(`DESCRIPTION\t${description}`);
+				break;
+			default:
+				console.log(`NAME\n\t${name}\nSYNOPSIS\n\t${synopsis}\nDESCRIPTION${description}`);
+				break;
+			}
 		}
-
-		console.log(`description: ${description}\nparams: ${parameters}`);
-
 	},
 };
