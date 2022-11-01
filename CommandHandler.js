e28aadcf72529cf253a93bfd6a98f1ed8f973929
MatterMr <48224224+MatterMr@users.noreply.github.com>
2022-10-31 21:51:06 -0600

    Added Manual Loader/Formater

--- CommandHandler.js
+++ CommandHandler.js
@@ -6,7 +6,9 @@ const path = require('node:path');
 class CommandHandler {
 	constructor() {
 		this.commandsPath = path.join(__dirname, 'commands');
+		this.manualsPath = path.join(this.commandsPath, 'manuals');
 		this.commandFiles = fs.readdirSync(this.commandsPath).filter(file => file.endsWith('.js'));
+		this.manualsFiles = fs.readdirSync(this.manualsPath).filter(file => file.endsWith('.json'));
 		this.commands = new Map();
 		this.setCommands();
 	}
@@ -14,12 +16,44 @@ class CommandHandler {
      * Loads all the commands from the commands directory
      */
 	setCommands() {
+		const manuals = this.loadManuals();
 		for (const file of this.commandFiles) {
 			const filePath = path.join(this.commandsPath, file);
 			const command = require(filePath);
+			command.manual = manuals.get(command.name);
 			this.commands.set(command.name, command);
 		}
 	}
+	/**
+     * @returns Map of all command manuals
+     */
+	loadManuals() {
+		const manuals = new Map();
+		for (const file of this.manualsFiles) {
+			const filePath = path.join(this.manualsPath, file);
+			const raw = fs.readFileSync(filePath);
+			const manual = JSON.parse(raw);
+			manuals.set(manual.name, this.formatManual(manual));
+		}
+		return manuals;
+	}
+	/**
+     * Formats the manual from a parsed JSON, and fixes multiline issues with JSON conversions.
+     * @param {Object} manual
+     * @returns Object
+     */
+	formatManual(manual) {
+		for (const key in manual) {
+			if (Array.isArray(manual[key])) {
+				let formatted = '';
+				for (const line of manual[key]) {formatted += '\t' + line + '\n';}
+				manual[key] = formatted;
+			}
+			else if (key != 'name') {manual[key] = '\t' + manual[key] + '\n';}
+		}
+		return manual;
+	}
+
 	/**
      * Manages the execution of commands based on string inputs, also converts strings formatted with brackets into objects
      * @param {String} input
