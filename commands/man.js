7be32da370c89d67e498a73a75d6e7744304febf
MatterMr <48224224+MatterMr@users.noreply.github.com>
2022-10-31 21:48:50 -0600

    Fixed Man to use JSON to store manuals

--- commands/man.js
+++ commands/man.js
@@ -1,14 +1,8 @@
 module.exports = {
 	'name': 'man',
-	'synopsis':'man [OPTION] [COMMAND_NAME]',
-	'description': `
-        Print manual for specifyed COMMAND or execute acording to OPTIONS.
-        -l : List all available commands
-        -s : Display only the synopsis of the command
-        -d : Dsiplay only the description of the command`,
+	'manual': null,
 	async execute(args, handler) {
 		const shift = args[1] == undefined ? 0 : 1;
-
 		if (args[0] == '-l') {
 			let formatted_string = 'Commands:\n';
 			for (const [key] of handler.commands) {
@@ -17,21 +11,42 @@ module.exports = {
 			console.log(formatted_string);
 		}
 		if (handler.commands.has(args[shift])) {
-			const name = handler.commands.get(args[shift]).name;
-			const synopsis = handler.commands.get(args[shift]).synopsis;
-			const description = handler.commands.get(args[shift]).description;
-
+			const manual = handler.commands.get(args[shift]).manual;
+			if (manual == null) {
+				console.log('No manual exists');
+				return;
+			}
 			switch (args[0]) {
 			case '-s':
-				console.log(`SYNOPSIS\n\t${synopsis}`);
+			case '-synopsis':
+				console.log(`#SYNOPSIS\n${manual.synopsis}`);
 				break;
 			case '-d':
-				console.log(`DESCRIPTION\t${description}`);
+			case '-description':
+				console.log(`#DESCRIPTION\n${manual.description}`);
+				break;
+			case '-o':
+			case '-options':
+				console.log(`#OPTIONS\n${manual.options}`);
+				break;
+			case '-e':
+			case '-examples':
+				console.log(`#EXAMPLES\n${manual.examples}`);
+				break;
+			case '':
+				console.log(
+					`#NAME\n\t${manual.name}\n` +
+                        `#SYNOPSIS\n${manual.synopsis}` +
+                        `#DESCRIPTION\n${manual.description}` +
+                        `#OPTIONS\n${manual.options}` +
+                        `#EXAMPLES\n${manual.examples}`);
 				break;
 			default:
-				console.log(`NAME\n\t${name}\nSYNOPSIS\n\t${synopsis}\nDESCRIPTION${description}`);
-				break;
+				console.log('Invalid Option');
 			}
+		}
+		else {
+			console.log('Invalid Command');
 		}
 	},
 };
